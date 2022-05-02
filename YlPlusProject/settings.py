ecae205f1c5ecd5b6d9ca2dd4ce68db9eec43a43
PatriotRossii <patriotrossii2019@mail.ru>
2022-05-02 22:47:07 +0500

    Load SECRET_KEY and DEBUG from environment variables

--- YlPlusProject/settings.py
+++ YlPlusProject/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-uf+3h=5vfyow@)3ftx$+2929()9^^gm=eqdl-idbr+jayukv+)"
+SECRET_KEY = environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
