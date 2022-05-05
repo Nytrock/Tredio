2ac485dc8c2f283ec324848ea2400794ccf02225
PatriotRossii <patriotrossii2019@mail.ru>
2022-05-05 16:15:10 +0500

    Implement theater-related models

--- theatres/models.py
+++ theatres/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Troupe(models.Model):
+    pass
+
+
+class TroupeMember(models.Model):
+    troupe_id = models.ForeignKey(Troupe, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100)
+
+
+class Location(models.Model):
+    """
+    Информация о местоположении.
+    Полем `query` следует пользоваться лишь в тех случаях, когда идентификатор ФИАС устарел.
+    """
+
+    #: Адрес одной строкой
+    query = models.CharField(max_length=250)
+
+    #: Уникальный идентификатор ФИАС
+    fias = models.CharField(max_length=50)
+
+
+class Theatre(models.Model):
+    name = models.CharField(max_length=150)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL)
+    troupe = models.ForeignKey(Troupe, on_delete=models.SET_NULL)
+    reviews = models.ForeignKey(ReviewGroup, on_delete=models.SET_NULL)
