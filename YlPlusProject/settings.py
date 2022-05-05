570cdde58383b28a72cd876dbd74c07e081b3c23
Nytrock <68463923+Nytrock@users.noreply.github.com>
2022-05-05 17:36:02 +0300

    Добавлены стили для ряда страниц

--- YlPlusProject/settings.py
+++ YlPlusProject/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jfbjdfbegnbeetyr3746fcbg'
+SECRET_KEY = environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "True"
+DEBUG = environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
