955fb93f9280fe6f67bb982bbc71cf6c12dc5af6
PatriotRossii <patriotrossii2019@mail.ru>
2022-05-11 15:58:27 +0500

    Implement rating admin, add names to fields and metadata to models

--- rating/admin.py
+++ rating/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from rating.models import Review, ReviewCategory, ReviewGroup, ReviewRating
+
+
+@admin.register(ReviewGroup)
+class ReviewGroupAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+
+
+@admin.register(ReviewCategory)
+class ReviewCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    fields = ("name",)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "review_group_id",
+        "category",
+        "star",
+    )
+    fields = (
+        "review_group_id",
+        "user",
+        "category",
+        "star",
+        "content",
+    )
+
+
+@admin.register(ReviewRating)
+class ReviewRatingAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "review",
+        "user",
+        "star",
+    )
+    fields = (
+        "review",
+        "user",
+        "star",
+    )
--- rating/models.py
+++ rating/models.py
@@ -9,20 +9,36 @@ User = get_user_model()
 class ReviewGroup(models.Model):
     pass
 
+    class Meta:
+        verbose_name = "Группа отзывов"
+        verbose_name_plural = "Группы отзывов"
+
 
 class ReviewCategory(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField("Название", max_length=100)
+
+    class Meta:
+        verbose_name = "Категория отзыва"
+        verbose_name_plural = "Категории отзывов"
 
 
 class Review(models.Model):
-    review_group_id = models.ForeignKey(ReviewGroup, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(ReviewCategory, on_delete=models.CASCADE)
-    star = models.IntegerField(validators=[RangeValidator(1, 5)])
-    content = models.CharField(max_length=2500, null=True, blank=True)
+    review_group_id = models.ForeignKey(ReviewGroup, verbose_name="Группа отзывов", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    category = models.ForeignKey(ReviewCategory, verbose_name="Категория", on_delete=models.CASCADE)
+    star = models.IntegerField("Оценка", validators=[RangeValidator(1, 5)])
+    content = models.CharField("Содержание", max_length=2500, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
 
 
 class ReviewRating(models.Model):
-    review = models.ForeignKey(Review, related_name="ratings", on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    star = models.BooleanField()
+    review = models.ForeignKey(Review, verbose_name="Отзыв", related_name="ratings", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    star = models.BooleanField("Оценка")
+
+    class Meta:
+        verbose_name = "Оценка отзыва"
+        verbose_name_plural = "Оценки отзывов"
