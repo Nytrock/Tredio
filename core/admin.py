e78f86b2317135f20e2a475fb12c9ad430e19fc7
PatriotRossii <patriotrossii2019@mail.ru>
2022-05-11 11:01:15 +0500

    Implement core admin, add names to fields and metadata to models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Contact, ContactsGroup, ContactType
+
+
+@admin.register(ContactsGroup)
+class ContactsGroupAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+
+
+@admin.register(ContactType)
+class ContactTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+    fields = ("name",)
+
+
+@admin.register(Contact)
+class Contact(admin.ModelAdmin):
+    list_display = ("id", "contacts_group_id", "type", "value")
+    fields = (
+        "contacts_group_id",
+        "type",
+        "value",
+    )
--- core/models.py
+++ core/models.py
@@ -2,14 +2,24 @@ from django.db import models
 
 
 class ContactsGroup(models.Model):
-    pass
+    class Meta:
+        verbose_name = "Группа контактов"
+        verbose_name_plural = "Группы контактов"
 
 
 class ContactType(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField("Название", max_length=100)
+
+    class Meta:
+        verbose_name = "Тип контакта"
+        verbose_name_plural = "Типы контактов"
 
 
 class Contact(models.Model):
-    contacts_group_id = models.ForeignKey(ContactsGroup, on_delete=models.CASCADE)
-    type = models.ForeignKey(ContactType, on_delete=models.CASCADE)
-    value = models.CharField(max_length=100)
+    contacts_group_id = models.ForeignKey(ContactsGroup, verbose_name="Группа контактов", on_delete=models.CASCADE)
+    type = models.ForeignKey(ContactType, verbose_name="Тип контакта", on_delete=models.CASCADE)
+    value = models.CharField("Значение", max_length=100)
+
+    class Meta:
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
