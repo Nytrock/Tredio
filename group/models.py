936142f47d882af76a912b3cabf81ae2f400dde0
PatriotRossii <patriotrossii2019@mail.ru>
2022-05-11 15:46:55 +0500

    Implement group admin, add names to fields and metadata to models

--- group/models.py
+++ group/models.py
@@ -8,13 +8,23 @@ User = get_user_model()
 
 
 class Meetup(models.Model):
-    host = models.ForeignKey(User, on_delete=models.CASCADE)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    start = models.DateTimeField()
-    participants_limit = models.IntegerField(validators=[MinValueValidator(1)], null=True, blank=True)
-    description = models.CharField(max_length=2500, null=True, blank=True)
+    host = models.ForeignKey(User, verbose_name="Организатор", on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, verbose_name="Событие", on_delete=models.CASCADE)
+    start = models.DateTimeField(verbose_name="Время встречи")
+    participants_limit = models.IntegerField(
+        "Максимальное кол-во участников", validators=[MinValueValidator(1)], null=True, blank=True
+    )
+    description = models.CharField("Описание", max_length=2500, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Встреча"
+        verbose_name_plural = "Встречи"
 
 
 class MeetupParticipant(models.Model):
-    meetup = models.ForeignKey(Meetup, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    meetup = models.ForeignKey(Meetup, verbose_name="Встреча", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name="Участник", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Участник встречи"
+        verbose_name_plural = "Участники встреч"
