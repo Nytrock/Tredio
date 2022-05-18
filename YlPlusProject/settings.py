47cc87c0cffdd6dbd8b6934f48b8b8362444385b
Artem <artem00561@gmail.com>
2022-05-18 16:35:32 +0300

    add
    Took 8 minutes

--- YlPlusProject/settings.py
+++ YlPlusProject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 
 # Application definition
