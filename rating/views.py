0a3e9077dc89f99b3998d71445f0772511e8ec9d
PatriotRossii <patriotrossii2019@mail.ru>
2022-05-19 09:08:46 +0500

    Add stub for RatingCreateView

--- rating/views.py
+++ rating/views.py
@@ -2,12 +2,19 @@ from django.views.generic import TemplateView
 
 
 class RatingTheatreView(TemplateView):
-    template_name = 'rating/rating_theatre.html'
+    template_name = "rating/rating_theatre.html"
 
 
 class RatingEventView(TemplateView):
-    template_name = 'rating/rating_event.html'
+    template_name = "rating/rating_event.html"
 
 
 class RatingCreateView(TemplateView):
-    template_name = 'rating/rating_create.html'
+    template_name = "rating/rating_create.html"
+
+    def get_context_data(self, **kwargs):
+        # Replace None with real form
+
+        context = super().get_context_data(**kwargs)
+        context["form"] = None
+        return context
