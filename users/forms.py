04a5018a38151385dccee5d70e75c2bfea25cf24
PatriotRossii <patriotrossii2019@mail.ru>
2022-05-21 12:32:41 +0500

    Use correct first_name and last_name fields in ChangeMainProfileForm, deduplicate

--- users/forms.py
+++ users/forms.py
@@ -6,29 +6,33 @@ from users.models import UserProfile
 
 User = get_user_model()
 
+FIRST_NAME_FIELD = forms.CharField(
+    label="Имя",
+    widget=widgets.Textarea(
+        attrs={
+            "maxlength": UserProfile._meta.get_field("first_name").max_length,
+            "class": "form-control",
+            "placeholder": "Имя",
+        }
+    ),
+)
+
+LAST_NAME_FIELD = forms.CharField(
+    label="Фамилия",
+    widget=widgets.Textarea(
+        attrs={
+            "maxlength": UserProfile._meta.get_field("last_name").max_length,
+            "class": "form-control",
+            "placeholder": "Фамилия",
+        }
+    ),
+)
+
 
 class CustomUserCreationForm(ModelForm):
-    first_name = forms.CharField(
-        label="Имя",
-        widget=widgets.Textarea(
-            attrs={
-                "maxlength": UserProfile._meta.get_field("first_name").max_length,
-                "class": "form-control",
-                "placeholder": "Имя",
-            }
-        ),
-    )
+    first_name = FIRST_NAME_FIELD
 
-    last_name = forms.CharField(
-        label="Фамилия",
-        widget=widgets.Textarea(
-            attrs={
-                "maxlength": UserProfile._meta.get_field("last_name").max_length,
-                "class": "form-control",
-                "placeholder": "Фамилия",
-            }
-        ),
-    )
+    last_name = LAST_NAME_FIELD
 
     password1 = forms.CharField(
         label="Пароль",
@@ -115,13 +119,15 @@ class ChangeMainProfileForm(ModelForm):
         ),
     )
 
+    first_name = FIRST_NAME_FIELD
+
+    last_name = LAST_NAME_FIELD
+
     class Meta:
         model = User
-        fields = (User.email.field.name, User.first_name.field.name, User.last_name.field.name)
+        fields = (User.email.field.name,)
         labels = {
             User.email.field.name: "Почта",
-            User.first_name.field.name: "Имя",
-            User.last_name.field.name: "Фамилия",
         }
 
 
