99fed639cad7e275aabdce0653102685eb9f2360
PatriotRossii <patriotrossii2019@mail.ru>
2022-05-21 13:47:57 +0500

    Update rank if experience is updated

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from fieldsignals import pre_save_changed
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "users"
+
+    def ready(self):
+        from .models import UserProfile, update_rank
+
+        pre_save_changed.connect(receiver=update_rank, sender=UserProfile, fields=["experience"])
