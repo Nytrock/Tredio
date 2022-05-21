d265ea6a21200fbd9488fa9b7d5ec84c2016f6e5
PatriotRossii <patriotrossii2019@mail.ru>
2022-05-21 12:34:06 +0500

    Make rank field optional

--- users/models.py
+++ users/models.py
@@ -100,7 +100,7 @@ class UserProfile(CommonProfile):
     user = models.OneToOneField(
         User, verbose_name="Пользователь", on_delete=models.CASCADE, related_name="user_profile"
     )
-    rank = models.ForeignKey(Rank, verbose_name="Ранг", on_delete=models.RESTRICT)
+    rank = models.ForeignKey(Rank, verbose_name="Ранг", on_delete=models.RESTRICT, null=True, blank=True)
     experience = models.IntegerField("Опыт", default=0)
 
     objects = models.Manager()
