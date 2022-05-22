ef97dbc3d2074bc95b4eb5fdc268f3ec945e2b6a
Nytrock <68463923+Nytrock@users.noreply.github.com>
2022-05-22 16:51:37 +0300

    I'm idiot
    Fix server

--- YlPlusProject/settings.py
+++ YlPlusProject/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "dhgfbrdthbng"
+SECRET_KEY = environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "True"
+DEBUG = environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 
 # Application definition
