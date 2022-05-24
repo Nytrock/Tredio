a413b8093c86a0c0c9e81945d6067eadb55b4c89
PatriotRossii <patriotrossii2019@mail.ru>
2022-05-24 21:15:24 +0500

    Implement address validator

--- core/validators.py
+++ core/validators.py
@@ -1,3 +1,5 @@
+from dadata import Dadata
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils.deconstruct import deconstructible
@@ -18,3 +20,22 @@ class RangeValidator(object):
 
     def __eq__(self, other):
         return self.min_value == other.min_value and self.max_value == other.max_value
+
+
+@deconstructible
+class AddressValidator(object):
+    def __init__(self, query: str, fias: str):
+        self.query = query
+        self.fias = fias
+
+    def __call__(self):
+        with Dadata(settings.DADATA_API_KEY, settings.DADATA_SECRET_KEY) as dadata:
+            response = dadata.find_by_id(name="address", query=self.fias)
+
+            if not response:
+                raise ValidationError(f"ФИАС идентификатор {self.fias} не существует")
+            if response[0].query != self.query:
+                raise ValidationError(f"ФИАС идентификатор не соответствует адресу")
+
+    def __eq__(self, other):
+        return self.query == other.query and self.fias == other.fias
